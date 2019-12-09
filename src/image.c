e5142696b1ecef678f9d4d97560c9e7bf1ff703a
Sylvain Jalbert <jalbertsyl@eisti.fr>
2019-12-09 10:23:53 +0100

    charger une image

--- src/image.c
+++ src/image.c
@@ -2,7 +2,33 @@
 \file image.c
 \autor Jalbert Sylvain
 \version 1
-\date 29 novembre 2019
+\date 02 decembre 2019
 \brief le fichier qui contient les définitions de toutes les méthodes relatives aux manipulations d'image
 */
 #include "image.h"
+
+/*!
+\fn FILE *chargerImage(char *tchar_nomFichier)
+\author Jalbert Sylvain
+\version 0.1 Premier jet
+\date 02 decembre 2019
+\brief la fonction qui charge une image
+\param tchar_nomFichier le nom du fichier à ouvrir
+\return le pointeur vers le fichier ouvert
+*/
+FILE *chargerImage(char *tchar_nomFichier){
+  int int_retour; //ce que retourne la fonction d'ouverture de fichier
+  FILE *file_image; //l'image qui sera ouverte
+
+  //ouverture de l'image en ecriture / lecture
+  file_image = fopen(tchar_nomFichier, "w+");
+  if (pfil_fic == NULL) {
+		// Si pb alors on affiche un message
+		fprintf(file_image, "Problème d'ouverture du fichier : %s\n", strerror (errno));
+		// et on quitte
+		exit(ERREUR_SYS);
+	}
+
+  //retourner l'image chargé
+  return(file_image);
+}
--- src/image.h
+++ src/image.h
@@ -2,12 +2,30 @@
 \file image.h
 \autor Jalbert Sylvain
 \version 1
-\date 29 novembre 2019
+\date 02 decembre 2019
 \brief le fichier qui contient les déclarations de toutes les méthodes relatives aux manipulations d'image
 */
 
 #ifndef __IMAGE_H_
 #define __IMAGE_H_
 
+#include <stdio.h>
+
+/*! \def ERREUR_SYS
+ *  Constante pour définir une terminaison par erreur
+ *  système
+ */
+#define ERREUR_SYS -1
+
+/*!
+\fn FILE *chargerImage(char *tchar_nomFichier)
+\author Jalbert Sylvain
+\version 0.1 Premier jet
+\date 02 decembre 2019
+\brief la fonction qui charge une image
+\param tchar_nomFichier le nom du fichier à ouvrir
+\return le pointeur vers le fichier ouvert
+*/
+FILE *chargerImage(char *tchar_nomFichier)
 
 #endif
